5627f7295656c906d722455129449af8d21a7c34
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:11:03 +0200

    "Add Next.js Image, Link, and utility imports"

---
+++ frontend/components/moviecard.js
@@ -0,0 +1,5 @@
+import Image from "next/image";
+import Link from "next/link";
+import { cx } from "@/utils/all";
+import { StarIcon } from "@sanity/icons";
+import { IconInnerShadowTopRight, IconThumbUp } from "@tabler/icons-react";
