ab8d0605f99172d617b81323b3ffd8174703fd3a
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:11:22 +0200

    "Add basic MovieCard component structure"

--- frontend/components/moviecard.js
+++ frontend/components/moviecard.js
@@ -3,3 +3,19 @@ import Link from "next/link";
 import { cx } from "@/utils/all";
 import { StarIcon } from "@sanity/icons";
 import { IconInnerShadowTopRight, IconThumbUp } from "@tabler/icons-react";
+
+export default function MovieCard({
+  movie,
+  aspect,
+  minimal,
+  fontSize,
+  fontWeight
+}) {
+  console.log(movie);
+  return (
+    <>
+      <div
+        className={cx(
+          "group cursor-pointer",
+          minimal && "grid gap-10 md:grid-cols-2"
+        )}>
