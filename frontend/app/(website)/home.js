7f35e9c18a2b493546eb03f16a15f24655c32f3f
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:31:19 +0200

    "Add fetchSearchResults function to get movie recommendations and details"

--- frontend/app/(website)/home.js
+++ frontend/app/(website)/home.js
@@ -9,3 +9,40 @@ export default function Post() {
   const [movieDetails, setMovieDetails] = useState([]);
   const [searchVal, setSearchVal] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+    // Function to fetch recommendations and then fetch movie details from OMDB
+    const fetchSearchResults = async () => {
+      try {
+        const response = await fetch(
+          "http://127.0.0.1:5000/recommend",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ movie_name: searchVal })
+          }
+        );
+        if (!response.ok)
+          throw new Error(`HTTP error! status: ${response.status}`);
+        const data = await response.json();
+        setIsLoading(true);
+        const movieDetailsPromises = data.recommendations.map(movie =>
+          fetch(
+            `http://www.omdbapi.com/?t=${encodeURIComponent(
+              movie
+            )}&apikey=57408735`
+          ).then(res => {
+            if (!res.ok) throw new Error("Failed to fetch from OMDB");
+            return res.json();
+          })
+        );
+  
+        const moviesDetails = await Promise.all(movieDetailsPromises);
+        setMovieDetails(moviesDetails);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+  
