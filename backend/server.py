5b665c0d5f4693e5d839266af34e365711ff21c8
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 14:32:47 +0200

    "Load similarity matrix from pickle file"

--- backend/server.py
+++ backend/server.py
@@ -8,3 +8,7 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 final_data = pd.read_csv('movie_dataset.csv')
 final_data['movie_title'] = final_data['movie_title'].str.strip().str.lower()
+
+with open('similarity.pkl', 'rb') as file:
+    similarity = pickle.load(file)
+
