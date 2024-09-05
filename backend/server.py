a74f5d6b2abbbe541fa178d7e58930262a6fe908
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 14:32:02 +0200

    "Initialize Flask app and configure CORS"

--- backend/server.py
+++ backend/server.py
@@ -2,3 +2,6 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pickle
 import pandas as pd
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
