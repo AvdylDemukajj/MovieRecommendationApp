c6b44088ff225c2dd74849abd9fbef64b050c7ef
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:16:57 +0200

    "Add logo and menu items for Navbar"

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -35,3 +35,51 @@ export default function Navbar(props) {
           {({ open }) => (
             <>
               <div className="flex flex-wrap justify-between md:flex-nowrap md:gap-10">
+              <div className="order-1 hidden w-full flex-col items-center justify-start md:order-none md:flex md:w-auto md:flex-1 md:flex-row md:justify-end">
+                  {leftmenu.map((item, index) => (
+                    <Fragment key={`${item.label}${index}`}>
+                      {item.children && item.children.length > 0 ? (
+                        <DropdownMenu
+                          menu={item}
+                          key={`${item.label}${index}`}
+                          items={item.children}
+                        />
+                      ) : (
+                        <Link
+                          href={item.href}
+                          key={`${item.label}${index}`}
+                          className="px-5 py-2 text-sm font-medium text-gray-600 hover:text-blue-500 dark:text-gray-400"
+                          target={item.external ? "_blank" : ""}
+                          rel={item.external ? "noopener" : ""}>
+                          {item.label}
+                        </Link>
+                      )}
+                    </Fragment>
+                  ))}
+                </div>
+                <div className="flex w-full items-center justify-between md:w-auto">
+                  <Link href="/" className="w-28 dark:hidden">
+                    {props.logo ? (
+                      <Image
+                        {...urlForImage(props.logo)}
+                        alt="Logo"
+                        priority={true}
+                        sizes="(max-width: 640px) 100vw, 200px"
+                      />
+                    ) : (
+                      <div className="flex flex-col items-center">
+                        <lord-icon
+                          src="https://cdn.lordicon.com/tlvdupnp.json"
+                          trigger="loop"
+                          delay="2000"
+                          style={{
+                            width: "40px",
+                            height: "40px"
+                          }}></lord-icon>
+                        <span className={`font-sacramento  text-3xl font-bold`}>
+                          CineMatch
+                        </span>
+                      </div>
+                    )}
+                  </Link>
+
