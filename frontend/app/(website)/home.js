274b6d7da76730bb33c46a052cf1e0d7a7aacaa2
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:32:33 +0200

    "Build UI for movie search and display results with loading indicator"

--- frontend/app/(website)/home.js
+++ frontend/app/(website)/home.js
@@ -60,4 +60,69 @@ export default function Post() {
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [searchVal]);
 
-  
+  return (
+    // Maleficent
+    <>
+      {movieDetails && (
+        <Container>
+          <div className="relative flex h-12 w-full items-center overflow-hidden rounded-lg bg-white focus-within:shadow-lg">
+            <div className="grid h-full w-12 place-items-center text-gray-300">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </div>
+
+            <input
+              className="peer h-full w-full pr-2 text-sm text-gray-700 outline-none dark:bg-gray-900 dark:text-white"
+              type="text"
+              id="search"
+              onChange={e => {
+                setSearchVal(e.target.value);
+              }}
+              placeholder="Search something.."
+            />
+          </div>
+
+          {isLoading ? (
+            <div className="mt-12 flex flex-row items-center justify-center">
+              <lord-icon
+                src="https://cdn.lordicon.com/ybaojceo.json"
+                trigger="loop"
+                delay="2000"
+                style={{
+                  width: "40px",
+                  height: "40px"
+                }}></lord-icon>
+            </div>
+          ) : (
+            <div className="mt-10 grid gap-10 md:grid-cols-2 lg:gap-10 xl:grid-cols-3 ">
+              {movieDetails.map(movies => (
+                <MovieCard movie={movies} aspect="square" />
+              ))}
+            </div>
+          )}
+
+          <div className="mt-10 flex justify-center">
+            <lord-icon
+              src="https://cdn.lordicon.com/tfliqeqn.json"
+              trigger="loop"
+              style={{
+                width: "100px",
+                height: "100px"
+              }}></lord-icon>
+          </div>
+        </Container>
+      )}
+    </>
+  );
+}
