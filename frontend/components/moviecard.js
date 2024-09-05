87e74443b7306683ab590dc98e036bcfee09c902
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:11:41 +0200

    "Add Image rendering and link structure for movie poster"

--- frontend/components/moviecard.js
+++ frontend/components/moviecard.js
@@ -19,3 +19,27 @@ export default function MovieCard({
           "group cursor-pointer",
           minimal && "grid gap-10 md:grid-cols-2"
         )}>
+                  <div
+          className={cx(
+            "overflow-hidden rounded-md bg-gray-100 transition-all hover:scale-105 dark:bg-gray-800"
+          )}>
+          <Link
+            className={cx(
+              "relative block",
+              aspect === "landscape"
+                ? "aspect-video"
+                : aspect === "custom"
+                ? "aspect-[5/4]"
+                : "aspect-square"
+            )}
+            href={``}>
+            <Image
+              src={movie?.Poster}
+              alt={movie?.Poster}
+              className="object-cover transition-all"
+              fill
+              sizes="(max-width: 768px) 30vw, 33vw"
+            />
+          </Link>
+        </div>
+
