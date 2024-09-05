ef8dea6e9022bf59a77888bdb7eef6ed093cf91f
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:03:42 +0200

    "Add React, Next.js, and core component imports"

---
+++ frontend/components/layout.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
+import { urlForImage } from "@/lib/sanity/image";
+import Navbar from "@/components/navbar";
+import NavbarAlt from "@/components/navbaralt";
+import { cx } from "@/utils/all";
+import Footer from "@/components/footer";
