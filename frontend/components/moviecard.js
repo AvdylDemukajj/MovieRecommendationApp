9cb0e62b41c5523a501c87ae914f356a2fa85859
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 15:12:41 +0200

    "Add movie title and additional details section"

--- frontend/components/moviecard.js
+++ frontend/components/moviecard.js
@@ -62,5 +62,25 @@ export default function MovieCard({
                 </p>
               </div>
             </div>
+            <h2
+              className={cx(
+                fontSize === "large"
+                  ? "text-2xl"
+                  : minimal
+                  ? "text-3xl"
+                  : "text-lg",
+                fontWeight === "normal"
+                  ? "line-clamp-2 font-medium tracking-normal text-black"
+                  : "font-semibold leading-snug tracking-tight",
+                "mt-2 dark:text-white"
+              )}>
+              <Link href={``}>
+                <span
+                  className="bg-gradient-to-r from-green-200 to-green-100 bg-[length:0px_10px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500 hover:bg-[length:100%_3px] group-hover:bg-[length:100%_10px] dark:from-purple-800 dark:to-purple-900">
+                  {movie.Title}
+                </span>
+              </Link>
+            </h2>
+
 
 
