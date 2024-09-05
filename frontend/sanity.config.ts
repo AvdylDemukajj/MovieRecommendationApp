82c864d8e93f4c93e2d18fa3407c9712fd37e781
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-09-05 14:58:16 +0200

    "Import project configuration and settings schema"

--- frontend/sanity.config.ts
+++ frontend/sanity.config.ts
@@ -2,3 +2,6 @@ import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./lib/sanity/schemas";
+
+import { projectId, dataset, previewSecretId } from "./lib/sanity/config";
+import settings from "./lib/sanity/schemas/settings";
